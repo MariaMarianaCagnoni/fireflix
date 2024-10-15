834d61cf856236f7e4538222f8b2b977b7b972fd
Mariana <mariamariana.cagnoni@gmail.com>
2024-10-14 22:09:37 -0300

    Feat: add  domain,application and infrastructure modules .

---
+++ domain/src/main/java/dev/mariamariana/admin/catalogo/domain/Category.java
@@ -0,0 +1,6 @@
+package dev.mariamariana.admin.catalogo.domain;
+
+public class Category {
+
+
+}
--- src/main/java/dev/mariamariana/admin/catalogo/Main.java
+++
@@ -1,9 +0,0 @@
-package dev.mariamariana.admin.catalogo;
-
-
-
-public class Main {
-    public static void main(String[] args) {
-
-        }
-    }
