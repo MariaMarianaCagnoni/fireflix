82e906a3a6f930e7c13b1785c1733caf1e3b1483
Mariana <mariamariana.cagnoni@gmail.com>
2024-12-09 13:32:14 -0300

    test: fix the test

--- backend/ff-admin-do-catalogo/domain/src/test/java/dev/mariamariana/admin/catalogo/domain/category/CategoryTest.java
+++ backend/ff-admin-do-catalogo/domain/src/test/java/dev/mariamariana/admin/catalogo/domain/category/CategoryTest.java
@@ -1,5 +1,8 @@
 package dev.mariamariana.admin.catalogo.domain.category;
 
+
+import dev.mariamariana.admin.catalogo.domain.exceptions.DomainException;
+import dev.mariamariana.admin.catalogo.domain.validation.handler.ThrowsValidationHandler;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -25,20 +28,20 @@ public class CategoryTest {
     }
 
     @Test
-    public void givenAnInvalidNullName_whenCallNewCategoryAndValidate_thenShouldReciveError() {
+    public void givenAnInvalidNullName_whenCallNewCategoryAndValidate_thenShouldReceiveError() {
         final String expectedName = null;
-
-        final var expectedErrorMessage = "'name'should not be null";
         final var expectedErrorCount = 1;
+        final var expectedErrorMessage = "'name' should not be null";
         final var expectedDescription = "A categoria mais assistida";
         final var expectedIsActive = true;
-        final var actualCategory = Category.newCategory(expectedName, expectedDescription, expectedIsActive);
 
-        final var actualException = Assertions.assertThrows(DomainException.class, () -> actualCategory.validate());
+        final var actualCategory =
+                Category.newCategory(expectedName, expectedDescription, expectedIsActive);
+
+        final var actualException =
+                Assertions.assertThrows(DomainException.class, () -> actualCategory.validate(new ThrowsValidationHandler()));
 
         Assertions.assertEquals(expectedErrorCount, actualException.getErrors().size());
-        Assertions.assertEquals(expectedErrorMessage, actualException.getErrors().get(0));
-
-
+        Assertions.assertEquals(expectedErrorMessage, actualException.getErrors().get(0).message());
     }
 }
